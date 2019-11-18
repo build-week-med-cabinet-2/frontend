098460c4a21f001e98ef7df1a3d63b884aa7a2dc
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-18 13:15:07 -0700

    redux store being provided to the router and app. strainReducer scaffolded

---
+++ src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import { strainReducer as strains } from "./strainReducer";
+
+export default combineReducers({
+  strains
+});
---
+++ src/store/reducers/strainReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  strains: []
+};
+
+export const strainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
