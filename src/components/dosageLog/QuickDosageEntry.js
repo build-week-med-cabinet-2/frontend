01f6e0762330bdece9948c2a1f518b8fc2190435
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-19 11:37:31 -0700

    added QuickDosageLog & SavedStrainsList to Display

--- src/components/dosageLog/QuickDosageEntry.js
+++ src/components/dosageLog/QuickDosageEntry.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function QuickDosageEntry() {
-  return <div></div>;
+  return <div>Hello from quick dosage</div>;
 }
--- src/components/strainRecommender/SavedStrainsList.js
+++ src/components/strainRecommender/SavedStrainsList.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function SavedStrainsList() {
-  return <div></div>;
+  return <div>Hello from SavedStrainsList</div>;
 }
