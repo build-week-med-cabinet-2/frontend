cacea64804ea657aa4e8ae46bb89d40da25af959
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-19 14:51:10 -0700

    label and inputs create, changeHandler created

--- src/components/dosageLog/QuickDosageEntry.js
+++ src/components/dosageLog/QuickDosageEntry.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function QuickDosageEntry() {
-  return <div>Hello from quick dosage</div>;
+  const initialValues = {
+    date: "",
+    strain: "",
+    dosageAmount: "",
+    intakeMethod: ""
+  };
+
+  const [log, setLog] = useState(initialValues);
+
+  const handleChannge = e => {
+    setLog({ ...log, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="dosage date">Date:</label>
+        <input type="date" name="dosageDate" id="" />
+        <label htmlFor="strain">Strain:</label>
+        <input type="text" name="strain" id="" />
+        <label htmlFor="Dosage Amount">Dosage Amount:</label>
+        <input type="number" name="amount" id="" />
+        <label htmlFor="intake method">Intake Method:</label>
+        <input type="text" name="intake" id="" />
+      </form>
+    </>
+  );
 }
