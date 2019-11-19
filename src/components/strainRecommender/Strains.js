5f99463e3468ad2e141a398213bc188c062a21e6
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-19 16:35:50 -0700

    private strain route added

--- src/components/strainRecommender/Strains.js
+++ src/components/strainRecommender/Strains.js
@@ -2,8 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Strain from "./Strain";
 
-export default function Strains() {
+const Strains = () => {
   const strains = useSelector(state => state.strains.strains);
   console.log(strains);
   return <div></div>;
-}
+};
+
+export default Strains;
