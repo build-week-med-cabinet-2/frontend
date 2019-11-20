b249ad90d5dc666b431a5b8fe4edbe57e57e4e2c
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-19 16:00:02 -0800

    build out basics of a strain component, and added fake temp data to Strains file

--- src/components/strainRecommender/Strains.js
+++ src/components/strainRecommender/Strains.js
@@ -1,5 +1,43 @@
 import React from "react";
+import Strain from './Strain';
 
 export default function Strains() {
-  return <div></div>;
+
+  const tempStrains = [
+    {
+      name: 'OG Kush',
+      type: 'hybrid',
+      description: 'mild stuff',
+      benefit: 'helps with pain',
+      dosageRec: '5mg',
+      rating: 4,
+      comments: ['helped relieve my pain', 'would recommend']
+    },
+    {
+      name: 'Tangie',
+      type: 'sativa',
+      description: 'strong stuff',
+      benefit: 'helps with pain a lot',
+      dosageRec: '3mg',
+      rating: 3,
+      comments: ['helped with stuff', 'good, would recommend']
+    },
+    {
+      name: 'Bubba Kush',
+      type: 'indica',
+      description: 'pretty mild stuff',
+      benefit: 'helps with anxiety',
+      dosageRec: '8mg',
+      rating: 5,
+      comments: ['helped with my anxiety', 'pretty nice, recommend']
+    }
+  ]
+
+  return (
+    <div>
+      {tempStrains.map((item, index) => (
+         <Strain data={item} key={index}/>
+      ))}
+    </div>
+  )
 }
