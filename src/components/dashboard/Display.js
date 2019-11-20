d2b6e7ebeec47d602b0e164631b2b2b66c2d0797
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-20 11:09:43 -0700

    added Retake intake button

--- src/components/dashboard/Display.js
+++ src/components/dashboard/Display.js
@@ -2,12 +2,21 @@ import React from "react";
 import QuickDosageEntry from "../dosageLog/QuickDosageEntry";
 import SavedStrainsList from "../strainRecommender/SavedStrainsList";
 import Log from "../dosageLog/Log";
-export default function Display() {
+
+const Display = props => {
+  const routeChange = e => {
+    e.preventDefault();
+    let path = "/intake/ailments";
+    props.history.push(path);
+  };
   return (
     <div>
       <QuickDosageEntry />
       <Log />
       <SavedStrainsList />
+      <button onClick={routeChange}>Redo Intake</button>
     </div>
   );
-}
+};
+
+export default Display;
