5d0751b5d03a09026b40b49f1d8ecbdb68f894d7
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-19 17:51:11 -0700

    removed Strains from SavedStrainsList

--- src/components/strainRecommender/SavedStrainsList.js
+++ src/components/strainRecommender/SavedStrainsList.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import Strains from "./Strains";
-
 export default function SavedStrainsList() {
   return (
     <>
       <div>Users SaveStrains will go here</div>
-      <Strains />
     </>
-  )
+  );
 }
