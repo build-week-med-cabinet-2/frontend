3e2f1203df6a73e985c3fa7a1d8d18be694d166d
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-19 17:37:50 -0700

    straoms getting strains from redux

--- src/store/reducers/strainReducer.js
+++ src/store/reducers/strainReducer.js
@@ -2,10 +2,26 @@ const initialState = {
   strains: [
     {
       id: 0,
-      name: "Northern Lights",
-      type: "Indica",
-      desc: "The Cannabis Choice Of Creed Bratton",
-      benefits: "Comedy, Musical Talent, being forever turning 30 next November"
+      name: "OG Kush",
+      type: "hybrid",
+      description: "mild stuff",
+      benefit: "helps with pain",
+      dosageRec: "5mg"
+    },
+    {
+      id: 1,
+      name: "Tangie",
+      type: "sativa",
+      description: "strong stuff",
+      benefit: "helps with pain a lot",
+      dosageRec: "3mg"
+    },
+    {
+      name: "Bubba Kush",
+      type: "indica",
+      description: "pretty mild stuff",
+      benefit: "helps with anxiety",
+      dosageRec: "8mg"
     }
   ]
 };
