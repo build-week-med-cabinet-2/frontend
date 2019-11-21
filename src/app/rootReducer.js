a9ceb8b6050fe1d3a6b2eccf86f6a31e16f2034e
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-21 14:41:42 -0700

    recommendationSlice finished i think

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 
 import ailmentsReducer from "../features/ailments/ailmentsSlice";
+import recommendationReducer from "../features/strainRecommendations/recommendationSlice";
 import { strainReducer as strains } from "../features/strains/strainsSlice";
 import { dosageReducer as dosages } from "../redux/reducers/dosageReducer";
 import { loginReducer as logon } from "../redux/reducers/loginReducer";
@@ -8,6 +9,7 @@ import { loginReducer as logon } from "../redux/reducers/loginReducer";
 export default combineReducers({
   ailments: ailmentsReducer,
   strains,
+  recommendations: recommendationReducer,
   dosages,
   logon
 });
--- src/features/strainRecommendations/recommendationSlice.js
+++ src/features/strainRecommendations/recommendationSlice.js
@@ -2,10 +2,46 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const recomendationSlice = createSlice({
   name: "recommendations",
-  initialState: [],
+  initialState: {
+    recommendedStrains: [
+      {
+        id: 0,
+        name: "OG Kush",
+        type: "hybrid",
+        rating: 4.4,
+        effects: "Relaxed, Hungry, Uplifted",
+        description:
+          "The potency of this strain gives the consumer a one-two punch to the head and body. Its effects may help with managing nausea, stress, minor body aches, and sleeplessness."
+      },
+      {
+        id: 1,
+        name: "Tangie",
+        type: "sativa",
+        rating: 4.1,
+        effects: "Happy, Hungry, Focused",
+        description:
+          "The potency of this strain gives the consumer a one-two punch to the head and body. Its effects may help with managing nausea, stress, minor body aches, and sleeplessness."
+      },
+      {
+        name: "Bubba Kush",
+        type: "indica",
+        rating: 4.7,
+        effects: "Sleepy, Hungry, Uplifted",
+        description:
+          "The potency of this strain gives the consumer a one-two punch to the head and body. Its effects may help with managing nausea, stress, minor body aches, and sleeplessness."
+      }
+    ],
+    savedStrains: []
+  },
   reducers: {
-    addRecommendation(state, action) {},
-    removeRecommendation(state, action) {}
+    saveRecommendation(state, action) {
+      const strain = action.paylod;
+      console.log(strain);
+      state.savedStrains.push(strain);
+    },
+    removeRecommendation(state, action) {
+      state.savedStrains.filter(obj => obj.id !== action.payload.id);
+    }
   }
 });
 
